355972d96b340dba9a4a23f973e195325e947061
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-03-24 21:22:08 +0200

    add the main routes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,32 @@
-import React, { Component } from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect
+} from "react-router-dom";
 import "./App.css";
 
 // components
 import Movies from "./components/movies/movies";
+import Customers from "./components/layout/pages/customers/customers";
+import Rentals from "./components/layout/pages/rentals/rentals";
+import Movie from "./components/layout/pages/movie/movie";
+import NotFound from "./components/layout/pages/notFound/notFound";
 
-class App extends Component {
-  render() {
-    return <Movies />;
-  }
-}
+const App = () => {
+  return (
+    <Router>
+      <Switch>
+        <Route exact path="/movies" component={Movies} />
+        <Route path="/customers" component={Customers} />
+        <Route path="/rentals" component={Rentals} />
+        <Route path="/movie/:id" component={Movie} />
+        <Redirect from="/" to="/movies" />
+        <Route component={NotFound} />
+      </Switch>
+    </Router>
+  );
+};
 
 export default App;
