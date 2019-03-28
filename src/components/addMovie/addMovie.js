1384af0580f2a629bfde2ffa207e0c46850194a9
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-03-28 12:40:58 +0200

    styling AddMovie component and add BackLink component which allows returns to the movies

--- src/components/addMovie/addMovie.js
+++ src/components/addMovie/addMovie.js
@@ -8,6 +8,7 @@ import Joi from "joi-browser";
 
 // components
 import Form from "../form/form";
+import BackLink from "../layout/backWay/backLink";
 
 class AddMovie extends Form {
   state = {
@@ -40,15 +41,23 @@ class AddMovie extends Form {
   render() {
     const genres = getGenres();
     return (
-      <div className="col-sm-6 mx-auto">
-        <form onSubmit={this.handleSubmitForm} className="form">
-          {this.renderInput("title", "Title", true)}
-          {this.renderCustomSelectMenu("genre", genres, "Genre")}
-          {this.renderInput("stock", "Number in stock", true)}
-          {this.renderInput("rate", "Rate", true)}
-          {this.renderSubmit("Add", "btn-dark")}
-        </form>
-      </div>
+      <React.Fragment>
+        <BackLink />
+        <div className="col-md-6 mx-auto">
+          <div className="card bg-dark text-light border-light">
+            <h5 className="card-header bg-info">Add Movie</h5>
+            <div className="card-body">
+              <form onSubmit={this.handleSubmitForm} className="form">
+                {this.renderInput("title", "Title", true)}
+                {this.renderCustomSelectMenu("genre", genres, "Genre")}
+                {this.renderInput("stock", "Number in stock", true)}
+                {this.renderInput("rate", "Rate", true)}
+                {this.renderSubmit("Add", "btn-info")}
+              </form>
+            </div>
+          </div>
+        </div>
+      </React.Fragment>
     );
   }
 }
