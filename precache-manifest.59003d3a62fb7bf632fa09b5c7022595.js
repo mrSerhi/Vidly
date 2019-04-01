254002a99c7b6a4d9e9157cf2dd6824e70772254
Serhii Marushchenko <serhii.mar90@gmail.com>
2019-04-01 13:10:18 +0300

    Updates

---
+++ precache-manifest.59003d3a62fb7bf632fa09b5c7022595.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "c4774c194975ad94f4a8ea8de6002e3f",
+    "url": "/Vidly/static/media/Spin-1s-30px.c4774c19.svg"
+  },
+  {
+    "revision": "0478f9835e3f07eb5a79",
+    "url": "/Vidly/static/js/runtime~main.f4a75dff.js"
+  },
+  {
+    "revision": "1a5ac4fe755e85c3ebe9",
+    "url": "/Vidly/static/js/main.e1b0335c.chunk.js"
+  },
+  {
+    "revision": "2cf89d3cab2c4d6f030e",
+    "url": "/Vidly/static/js/2.29b7489b.chunk.js"
+  },
+  {
+    "revision": "1a5ac4fe755e85c3ebe9",
+    "url": "/Vidly/static/css/main.3a196fd6.chunk.css"
+  },
+  {
+    "revision": "2cf89d3cab2c4d6f030e",
+    "url": "/Vidly/static/css/2.b07087b3.chunk.css"
+  },
+  {
+    "revision": "3a84be64659068c150a0aacd466eae4b",
+    "url": "/Vidly/index.html"
+  }
+];
